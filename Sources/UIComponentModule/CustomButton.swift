eb48eafb8e25b5b36666c0bf2a204bcf1e782b22
Hendra Lijaya <hendralijaya@gmail.com>
2024-10-04 16:51:12 +0700

    Adding Button Components

---
+++ Sources/UIComponentModule/CustomButton.swift
@@ -0,0 +1,37 @@
+//
+//  RedButton.swift
+//  Servisa
+//
+//  Created by Nur Nisrina on 24/09/24.
+//
+
+import SwiftUI
+
+struct CustomButton: View {
+    let text: String
+    let isPrimary: Bool
+    let color: Color
+    let action: () -> ()
+    
+    var body: some View {
+        Button(action: {
+            action()
+        }) {
+            Text(text)
+                .font(.title3)
+                .fontWeight(.bold)
+                .frame(maxWidth: .infinity)
+        }
+        .padding()
+        .background(color)
+        .foregroundColor(.white)
+        .cornerRadius(14)
+        .padding()
+    }
+}
+
+#Preview {
+    CustomButton(text: "Ini button", isPrimary: true, color: .red) {
+        print("tes")
+    }
+}
