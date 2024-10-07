6566ead18f12167d47f9a04589a0c2abddb88acb
Balya Elfata <balyaelfata@yahoo.com>
2024-10-07 14:45:31 +0700

    added OnboardingPageIndicator component

---
+++ Sources/UIComponentModule/OnboardingPageIndicator.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  UIComponentModule
+//
+//  Created by Balya Elfata on 07/10/24.
+//
+
+import SwiftUI
+
+struct OnboardingPageIndicator: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingPageIndicator()
+}
