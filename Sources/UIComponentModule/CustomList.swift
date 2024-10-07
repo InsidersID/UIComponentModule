97275161253df3508b4d28833ce3bdcfd9a7eda2
Balya Elfata <balyaelfata@yahoo.com>
2024-10-07 15:26:30 +0700

    add component logic for custom list, checklist, and add item button

--- Sources/UIComponentModule/CustomList.swift
+++ Sources/UIComponentModule/CustomList.swift
@@ -1,11 +1,48 @@
 import SwiftUI
 
+enum ChecklistStatus {
+    case done
+    case undone
+}
+
 struct CustomList: View {
+    var height: CGFloat = 46
+    var text: String
+    var isChecklist: Bool = false
+    var checklistStatus: ChecklistStatus = .undone
+    var isAddItem: Bool = true
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            RoundedRectangle(cornerRadius: 16)
+                .frame(width: .infinity, height: isAddItem ? 48 : height)
+                .foregroundStyle(.white)
+                .shadow(color: .black.opacity(0.1), radius: 5, x: 0, y: 0)
+            
+            HStack {
+                if isAddItem {
+                    Image(systemName: "plus.circle.fill")
+                        .foregroundStyle(.blue)
+                        .font(.title)
+                }
+                
+                Text(text)
+                    .fontWeight(.semibold)
+                
+                Spacer()
+                
+                if isChecklist {
+                    Image(systemName: checklistStatus == .done ? "checkmark.circle.fill" : "exclamationmark.circle.fill")
+                        .font(.title2)
+                        .foregroundStyle(checklistStatus == .done ? .green : .yellow)
+                }
+            }
+            .padding(.horizontal)
+        }
+        .padding(.horizontal)
     }
 }
 
 #Preview {
-    CustomList()
+    CustomList(text: "Masukkan identitas baru")
 }
