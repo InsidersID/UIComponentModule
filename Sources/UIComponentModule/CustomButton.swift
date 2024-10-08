683638cf4a33118db13968409df96e51e0a87837
nisrinous <nisrinous@gmail.com>
2024-10-08 12:41:37 +0700

    fix custombutton

--- Sources/UIComponentModule/CustomButton.swift
+++ Sources/UIComponentModule/CustomButton.swift
@@ -11,26 +11,33 @@ public struct CustomButton: View {
 
     let text: String
     let color: Color
+    let font: CGFloat
     let cornerRadius: CGFloat
+    let paddingHorizontal: CGFloat
+    let paddingVertical: CGFloat
     let action: () -> ()
     
-    public init(text: String, color: Color, cornerRadius: CGFloat, action: @escaping () -> Void) {
+    public init(text: String, color: Color, font: CGFloat, cornerRadius: CGFloat, paddingHorizontal: CGFloat, paddingVertical: CGFloat, action: @escaping () -> Void) {
         self.text = text
         self.color = color
+        self.font = font
         self.action = action
         self.cornerRadius = cornerRadius
+        self.paddingHorizontal = paddingHorizontal
+        self.paddingVertical = paddingVertical
     }
     
     public var body: some View {
         Button(action: {
             action()
         }) {
-            Text(text)
-                .font(.title3)
+            Text(text.prefix(1).capitalized + text.dropFirst())
+                .font(.system(size: font))
                 .fontWeight(.bold)
                 .frame(maxWidth: .infinity)
         }
-        .padding()
+        .padding(.horizontal, paddingHorizontal)
+        .padding(.vertical, paddingVertical)
         .background(color)
         .foregroundColor(.white)
         .cornerRadius(cornerRadius)
@@ -39,7 +46,7 @@ public struct CustomButton: View {
 }
 
 #Preview {
-    CustomButton(text: "Ini button", color: .red, cornerRadius: 14) {
+    CustomButton(text: "ini button", color: .red, font: 12, cornerRadius: 14, paddingHorizontal: 4, paddingVertical: 8) {
         print("tes")
     }
 }
