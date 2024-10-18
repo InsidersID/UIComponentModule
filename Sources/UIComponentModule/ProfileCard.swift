57a845b8e2d12d87ca9ee6278130ec144313c7ff
Balya Elfata <balyaelfata@yahoo.com>
2024-10-18 16:44:40 +0700

    fix: changed card title color to primary

--- Sources/UIComponentModule/ProfileCard.swift
+++ Sources/UIComponentModule/ProfileCard.swift
@@ -43,6 +43,7 @@ public struct ProfileCard: View {
                     Text(name)
                         .font(.headline)
                         .fontWeight(.semibold)
+                        .foregroundStyle(.primary)
                         .multilineTextAlignment(.leading)
                     
                     Spacer()
