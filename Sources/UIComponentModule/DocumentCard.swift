fc8d843f59c8bfe173110388c6d4625dd1e9ea05
Balya Elfata <balyaelfata@yahoo.com>
2024-10-18 15:11:04 +0700

    feat: Add DocumentCard component

---
+++ Sources/UIComponentModule/DocumentCard.swift
@@ -0,0 +1,40 @@
+import SwiftUI
+
+public struct DocumentCard: View {
+    public var width: CGFloat = .infinity
+    public var height: CGFloat = 142
+    public var document: String
+    public var status: String
+    
+    public init(document: String, status: String) {
+        self.document = document
+        self.status = status
+    }
+    
+    public var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 24)
+                .frame(width: width, height: height)
+                .foregroundStyle(.white)
+            
+            VStack(alignment: .leading) {
+                Text(document)
+                    .padding(.bottom)
+                
+                Text(status)
+                    .foregroundStyle(.red)
+                    .padding(.bottom)
+                
+                Button("Upload") {
+                    //
+                }
+                .padding(.bottom)
+            }
+        }
+    }
+}
+
+#Preview {
+    DocumentCard(document: "KTP", status: "Belum Selesai")
+        .background(.gray)
+}
