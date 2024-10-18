96bbb5db09711c15a59a007658cca39045175dc9
nisrinous <nisrinous@gmail.com>
2024-10-18 16:43:14 +0700

    feat: add progress to pengajuan visa card commponent

---
+++ Sources/UIComponentModule/CustomGauge.swift
@@ -0,0 +1,18 @@
+//
+//  CustomGauge.swift
+//  UIComponentModule
+//
+//  Created by Nur Nisrina on 18/10/24.
+//
+
+import SwiftUI
+
+struct CustomGauge: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
