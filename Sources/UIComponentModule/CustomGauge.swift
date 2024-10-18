ae73eae01bad842f6f8792e8c775419e86bb9f25
nisrinous <nisrinous@gmail.com>
2024-10-18 16:46:17 +0700

    feat: Add progress to visa applivation card

--- Sources/UIComponentModule/CustomGauge.swift
+++ Sources/UIComponentModule/CustomGauge.swift
@@ -8,11 +8,32 @@
 import SwiftUI
 
 struct CustomGauge: View {
+    
+    var visaProgressPercentage: Double
+    var visaProgressColor: Color
+    var lineWidth: CGFloat
+    var size: CGFloat
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Circle()
+                .stroke(visaProgressColor.opacity(0.5), lineWidth: lineWidth)
+                .frame(width: size, height: size)
+            
+            Circle()
+                .trim(from: 0, to: visaProgressPercentage/100)
+                .stroke(visaProgressColor, style: StrokeStyle(lineWidth: lineWidth, lineCap: .round))
+                .rotationEffect(.degrees(-90))
+                .frame(width: size, height: size)
+                .animation(.easeInOut, value: visaProgressPercentage)
+            Text("\(Int(visaProgressPercentage))%")
+                .font(.system(size: 16, weight: .bold))
+                .foregroundColor(visaProgressColor)
+                .frame(width: size, height: size)
+        }
     }
 }
 
 #Preview {
-    SwiftUIView()
+    CustomGauge(visaProgressPercentage: 75, visaProgressColor: .pink, lineWidth: 10, size: 70)
 }
--- Sources/UIComponentModule/VisaApplicationCard.swift
+++ Sources/UIComponentModule/VisaApplicationCard.swift
@@ -5,42 +5,55 @@
 //  Created by Nur Nisrina on 07/10/24.
 //
 
+import HealthKit
 import SwiftUI
 
 public struct VisaApplicationCard: View {
     
     let visaType: String
     let country: String
-    let date: Date
-    let visaState: String
-    let badgeColor: Color
     let countries: [String]
+    let visaProgressPercentage: Double
+    let visaProgressColor: Color
     let action: () -> ()
     
-    public init(visaType: String, country: String, date: Date, visaState: String, badgeColor: Color, countries: [String], action: @escaping () -> Void) {
+    public init(visaType: String, country: String, countries: [String], visaProgressPercentage: Double, visaProgressColor: Color, action: @escaping () -> Void) {
         self.visaType = visaType
         self.country = country
-        self.date = date
-        self.visaState = visaState
-        self.badgeColor = badgeColor
         self.countries = countries
+        self.visaProgressPercentage = visaProgressPercentage
+        self.visaProgressColor = visaProgressColor
         self.action = action
     }
     
+    
     public var body: some View {
-        VStack(alignment: .leading){
-            Text("Pengajuan visa \(visaType.lowercased()) \(country.capitalized)")
-                .font(.headline)
-                .padding(.vertical, 1)
-            Text(countries.joined(separator: ", "))
-                .font(.subheadline)
-            Badge(paddingHorizontal: 8, paddingVertical: 8, text: visaState, backgroundColor: .pink, color: .white)
+        CardContainer(cornerRadius: 25) {
+            HStack(spacing: 20) {
+                VStack {
+                    Image(systemName: "pengajuanvisa-\(country)")
+                }
+                VStack(alignment: .leading){
+                    Text("Pengajuan visa")
+                        .font(.headline)
+                    Text("\(visaType.lowercased()) \(country.capitalized)")
+                        .font(.headline)
+                    Text(countries.joined(separator: ", "))
+                        .font(.subheadline)
+                        .foregroundStyle(.gray)
+                        
+                }
+                VStack {
+                    CustomGauge(visaProgressPercentage: visaProgressPercentage, visaProgressColor: visaProgressColor, lineWidth: 10, size: 70)
+                }
+            }
         }
     }
+    
 }
 
 #Preview {
-    VisaApplicationCard(visaType: "turis", country: "itali", date: Date(), visaState: "Belum selesai", badgeColor: .pink, countries: ["Italia", "Jerman"]) {
+    VisaApplicationCard(visaType: "turis", country: "itali", countries: ["italy", "jerman"], visaProgressPercentage: 75, visaProgressColor: .pink) {
         print("Card clicked")
     }
 }
