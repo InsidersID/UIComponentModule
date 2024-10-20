6f021274ba1d7dae0a17fdfef05a370b7551cf1c
Balya Elfata <balyaelfata@yahoo.com>
2024-10-20 18:16:39 +0700

    feat: added delete notification logic

--- Sources/UIComponentModule/NotificationCard.swift
+++ Sources/UIComponentModule/NotificationCard.swift
@@ -7,33 +7,41 @@
 
 import SwiftUI
 
-struct NotificationCard: View {
-    var body: some View {
+public struct NotificationCard: View {
+    @State public var deleteNotification: Bool = false
+    
+    public var body: some View {
         GeometryReader { proxy in
-            ZStack(alignment: .center) {
-                RoundedRectangle(cornerRadius: 24)
-                    .foregroundStyle(.white)
-                    .shadow(color: .black.opacity(0.1), radius: 10)
-                
-                HStack {
-                    Image(systemName: "checkmark.circle")
-                        .foregroundStyle(.green)
+            if deleteNotification {EmptyView()} else {
+                ZStack(alignment: .center) {
+                    RoundedRectangle(cornerRadius: 24)
+                        .foregroundStyle(.white)
+                        .shadow(color: .black.opacity(0.1), radius: 10)
                     
-                    VStack(alignment: .leading) {
-                        Text("Entry berhasil dibuat!")
-                        Text("Pengajuan bisa dilihat di tab Visaku")
-                            .foregroundStyle(.secondary)
-                            .font(.subheadline)
+                    HStack {
+                        Image(systemName: "checkmark.circle")
+                            .foregroundStyle(.green)
+                        
+                        VStack(alignment: .leading) {
+                            Text("Entry berhasil dibuat!")
+                            Text("Pengajuan bisa dilihat di tab Visaku")
+                                .foregroundStyle(.secondary)
+                                .font(.subheadline)
+                        }
+                        
+                        Spacer()
                     }
+                    .padding(.horizontal)
                     
-                    Spacer()
+                    Image(systemName: "xmark")
+                        .offset(x: proxy.size.width*0.32, y: -proxy.size.height*0.025)
+                        .foregroundStyle(.gray)
+                        .onTapGesture {
+                            deleteNotification = true
+                        }
                 }
-                .padding(.horizontal)
-                
-                Image(systemName: "xmark")
-                    .offset(x: proxy.size.width*0.32, y: -proxy.size.height*0.025)
+                .frame(width: proxy.size.width*0.8, height: proxy.size.height*0.1)
             }
-            .frame(width: proxy.size.width*0.8, height: proxy.size.height*0.1)
         }
     }
 }
