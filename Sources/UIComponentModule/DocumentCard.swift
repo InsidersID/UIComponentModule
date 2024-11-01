5767ab741b7eec9494019674c8e4dbd6f5e9b7b2
Balya Elfata <balyaelfata@yahoo.com>
2024-11-01 16:54:18 +0700

    feat: added dynamic document status and call to action

--- Sources/UIComponentModule/DocumentCard.swift
+++ Sources/UIComponentModule/DocumentCard.swift
@@ -1,41 +1,89 @@
 import SwiftUI
 
+public enum DocumentStatus {
+    case notStarted
+    case inProgress
+    case done
+}
+
 public struct DocumentCard: View {
     public var width: CGFloat = .infinity
     public var height: CGFloat
     public var document: String
-    public var status: String
+    public var status: DocumentStatus
     
-    public init(height: CGFloat = 142, document: String, status: String) {
+    private var statusText: String {
+        switch status {
+        case .notStarted:
+            return "Upload Dokumen"
+        case .inProgress:
+            return "4 dari 4 bagian belum diisi"
+        case .done:
+            return "Selesai"
+        }
+    }
+    
+    private var statusColor: Color {
+        switch status {
+        case .notStarted:
+            return .blue
+        case .inProgress:
+            return .red
+        case .done:
+            return .green
+        }
+    }
+    public init(height: CGFloat = 142, document: String, status: DocumentStatus = .inProgress) {
         self.height = height
         self.document = document
         self.status = status
     }
     
     public var body: some View {
-        ZStack(alignment: .leading) {
-            RoundedRectangle(cornerRadius: 24)
+        CardContainer(cornerRadius: 24) {
+            ZStack(alignment: .leading) {
+//                RoundedRectangle(cornerRadius: 24)
+//                    .frame(width: width, height: height)
+//                    .foregroundStyle(.white)
+                
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text(document)
+                            .font(.title2)
+                            .fontWeight(.semibold)
+                            .foregroundStyle(.black)
+                        
+                        Spacer()
+                        
+                        Text(statusText)
+                            .foregroundStyle(statusColor)
+                    }
+                    
+                    Spacer()
+                    
+                    VStack {
+                        HStack {
+                            Text(status == .done ? "Lihat form" : "Mulai")
+                                .foregroundStyle(.blue)
+                            
+                            Image(systemName: "chevron.right")
+                                .foregroundStyle(document == "Informasi tambahan" ? .blue : .secondary)
+                                .font(.title2)
+                                .fontWeight(.semibold)
+                        }
+                        
+                        if document == "Informasi tambahan" {
+                            Spacer()
+                        }
+                    }
+                }
+                .padding()
                 .frame(width: width, height: height)
-                .foregroundStyle(.white)
-            
-            VStack(alignment: .leading) {
-                Text(document)
-                    .font(.system(.title2, design: .serif, weight: .bold))
-                    .foregroundStyle(.black)
-                
-                Spacer()
-                
-                Text(status)
-                    .foregroundStyle(.red)
             }
-            .padding()
-            .frame(width: width, height: height)
         }
-        .shadow(color: .black.opacity(0.1), radius: 10)
     }
 }
 
 #Preview {
-    DocumentCard(document: "KTP", status: "Belum ada")
-        .background(.gray)
+    DocumentCard(document: "Informasi tambahan", status: .inProgress)
 }
