af06be6793485f16dc5c9aeef69c128c375ba36d
Balya Elfata <balyaelfata@yahoo.com>
2024-11-12 16:56:36 +0700

    style: changed font and sizing minorly

--- Sources/UIComponentModule/ProfileCard.swift
+++ Sources/UIComponentModule/ProfileCard.swift
@@ -10,7 +10,7 @@ public struct ProfileCard: View {
     public var isAddProfile: Bool
     public var isCompleted: Bool
     
-    public init(width: CGFloat = 170.5, height: CGFloat = 198, photoWidth: CGFloat = .infinity, photoHeight: CGFloat = 140, name: String, isAddProfile: Bool = false, isCompleted: Bool = false, imageData: Data = Data()) {
+    public init(width: CGFloat = .leastNormalMagnitude, height: CGFloat = .leastNormalMagnitude, photoWidth: CGFloat = 146.5, photoHeight: CGFloat = 140, name: String, isAddProfile: Bool = false, isCompleted: Bool = false, imageData: Data = Data()) {
         self.width = width
         self.height = height
         self.photoWidth = photoWidth
@@ -24,46 +24,44 @@ public struct ProfileCard: View {
     public var body: some View {
         CardContainer(cornerRadius: 24) {
             VStack {
-                if isAddProfile {
-                    ZStack {
-                        Color(red: 0.4, green: 0.83, blue: 0.91)
-                            .frame(width: photoWidth)
-                            .aspectRatio(1, contentMode: .fit)
-                            .cornerRadius(24)
-                        Image(systemName: "plus")
-                            .resizable()
-                            .frame(width: 68.5, height: 68.5)
-                            .foregroundStyle(.white)
-                    }
-                } else {
-                    if let image = UIImage(data: imageData) {
-                        Image(uiImage: image)
-                            .resizable()
-                            .frame(width: photoWidth)
-                            .background(.gray)
-                            .aspectRatio(1, contentMode: .fit)
-                            .cornerRadius(24)
+                VStack {
+                    if isAddProfile {
+                        ZStack {
+                            Color(red: 0.4, green: 0.83, blue: 0.91)
+                                .frame(width: photoWidth, height: photoHeight)
+                                .aspectRatio(1, contentMode: .fit)
+                                .cornerRadius(24)
+                            Image(systemName: "plus")
+                                .resizable()
+                                .frame(width: 68.5, height: 68.5)
+                                .foregroundStyle(.white)
+                        }
                     } else {
-                        Image("")
-                            .resizable()
-                            .frame(width: photoWidth)
-                            .background(.gray)
-                            .aspectRatio(1, contentMode: .fit)
-                            .cornerRadius(24)
+                        if let image = UIImage(data: imageData) {
+                            Image(uiImage: image)
+                                .resizable()
+                                .frame(width: photoWidth, height: photoHeight)
+                                .cornerRadius(24)
+                        } else {
+                            Image("")
+                                .resizable()
+                                .frame(width: photoWidth, height: photoHeight)
+                                .background(Color(red: 0.85, green: 0.85, blue: 0.85))
+                                .cornerRadius(24)
+                        }
+                        
                     }
- 
                 }
-                
-                Spacer()
+                .padding(.bottom)
                 
                 HStack {
                     Text(name)
-                        .font(Font.custom("Inter", size: 16))
+                        .font(Font.custom("Inter-SemiBold", size: 16))
                         .fontWeight(.semibold)
-                        .foregroundStyle(.black.opacity(0.75))
+                        .foregroundStyle(Color(red: 0.04, green: 0.04, blue: 0.04).opacity(0.75))
                         .multilineTextAlignment(.leading)
                     
-                    Spacer()
+//                    Spacer()
                     
                     if isCompleted == false && isAddProfile == false {
                         Image(systemName: "exclamationmark.circle.fill")
